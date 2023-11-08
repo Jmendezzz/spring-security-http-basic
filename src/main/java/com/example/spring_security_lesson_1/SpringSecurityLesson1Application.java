5191e0b6fbfaf6181e4e19804f96608897c28d6d
Juan Gerardo <jmendez1025@cue.edu.co>
2023-11-08 12:02:22 -0500

    lesson 1 finished

---
+++ src/main/java/com/example/spring_security_lesson_1/SpringSecurityLesson1Application.java
@@ -0,0 +1,13 @@
+package com.example.spring_security_lesson_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityLesson1Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringSecurityLesson1Application.class, args);
+  }
+
+}
